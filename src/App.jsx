3e052f63c53043ef86a62c3dd3133b9ef79c256c
Milind <milindchaudhary80@gmail.com>
2025-04-25 09:46:30 +0200

    Home and map button padded in a container

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
 import { Button } from './components';
-
 import SingleComponents from './layouts/examples/SingleComponents';
 import MapExample from './layouts/examples/Map';
 
 const App = () => (
   <Router>
     <div>
-      <Link to="/" ><Button>Home</Button></Link>
-      <Link to="/map-example" ><Button>Map</Button></Link>
-
+      <div style={{ display: 'flex', gap: '15px', padding: '10px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)' }}>
+        <Link to="/"><Button>Home</Button></Link>
+        <Link to="/map-example"><Button>Map</Button></Link>
+      </div>
       <Route exact path="/" component={SingleComponents} />
       <Route path="/map-example" component={MapExample} />
     </div>
   </Router>
-)
+);
 
 export default App;
